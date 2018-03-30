527dca419e8be1d8cd03c66bc521a0e4c8cf9ced
liuxiao_mini <liuxiao@liuxiao-mac-mini.local>
2018-03-30 17:06:33 +0800

    fix: fix reset shell 'soft reset' bug

--- src/shell/news_commit_reset.sh
+++ src/shell/news_commit_reset.sh
@@ -3,7 +3,7 @@
 # 新闻提交前reset一些修改：
 # android7.0抓包、 fabric、 云捕、 LeakCanary、 哨兵（内存占用浮层）
 
-git reset --soft HEAD news/src/main/res/xml/network_security_config.xml
-git reset --soft HEAD news/src/main/AndroidManifest.xml
-git reset --soft HEAD news/src/main/java/com/netease/nr/base/config/ConfigDebug.java
-git reset --soft HEAD news/src/main/java/com/netease/thirdsdk/SDKManager.java
+git reset HEAD news/src/main/res/xml/network_security_config.xml
+git reset HEAD news/src/main/AndroidManifest.xml
+git reset HEAD news/src/main/java/com/netease/nr/base/config/ConfigDebug.java
+git reset HEAD news/src/main/java/com/netease/thirdsdk/SDKManager.java
