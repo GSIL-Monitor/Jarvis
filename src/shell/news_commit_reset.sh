8385844b416b9ede164572908484979deb6f4fbe
liuxiao_mini <liuxiao@liuxiao-mac-mini.local>
2018-04-08 14:22:21 +0800

    upd: update news commit reset shell

--- src/shell/news_commit_reset.sh
+++ src/shell/news_commit_reset.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 ##
-# 新闻提交前reset一些修改：
-# android7.0抓包、 fabric、 云捕、 LeakCanary、 哨兵（内存占用浮层）
+# 新闻提交前reset一些修改
+# alias newsreset='${path}/shell/./news_commit_reset.sh'
 ##
 
+# Android 7.0抓包
 git reset HEAD news/src/main/res/xml/network_security_config.xml
 git reset HEAD news/src/main/AndroidManifest.xml
+# LeakCanary
 git reset HEAD news/src/main/java/com/netease/nr/base/config/ConfigDebug.java
+# fabric、云捕、哨兵
 git reset HEAD news/src/main/java/com/netease/thirdsdk/SDKManager.java
+# 内存占用浮层
+git reset HEAD news/src/main/java/com/netease/thirdsdk/sentry/SentrySendService.java
