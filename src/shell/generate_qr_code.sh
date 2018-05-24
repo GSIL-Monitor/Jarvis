5c80441b497535f2034c6f9a5b8f1c6fc9e68cc9
liuxiao_MacMini <liuxiao@liuxiao-mac-mini.local>
2018-05-24 10:53:46 +0800

    [upd] update generate qrcode shell: auto install brew & qrencode

--- src/shell/generate_qr_code.sh
+++ src/shell/generate_qr_code.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
-if [ $# == 0 ]; then
-    read -p "Enter message here:" message
+#若没有brew环境，安装brew
+if which brew > /dev/null ; then
+    echo "brew has been installed."
+else
+    echo "brew not installed, install brew first."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
+#若没有qrencode库，安装qrencode
+if which qrencode > /dev/null ; then
+    echo "qrencode has been installed."
+else
+    echo "qrencode not installed, install brew first"
+    brew install qrencode
+fi
+
+#处理参数，获取要生成二维码的信息
+if [ $# == 0 ]; then
+    read -p "Enter message here: " message
 elif [[ $1 == "help" ]] || [[ $1 == "--help" ]]; then
     echo "A tool for generating qr code."
     echo "Example usage:"
     echo "  qrcode \"http://www.163.com\""
-    echo "  qrcode \"n 55iw\""
+    echo "  qrcode \"n 55!W\""
     echo "Further help:"
     echo "  qrcode help"
     echo "  qrcode --help"
     exit
-
 elif [ $1 ]; then
     message=$1
 fi
 
+#打印二维码
 echo $message | qrencode -o - -t UTF8
